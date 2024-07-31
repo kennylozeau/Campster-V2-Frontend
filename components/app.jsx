3432542db7ba33c7bddb581cf18424cacd19d8b3
Josiah Leon <sjosiahleon@gmail.com>
2024-07-31 11:11:33 -0700

    converted campsite index to hook

--- components/app.jsx
+++ components/app.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute} from '../util/route_util';
-import LoginFormContainer from './auth/login_form_container';
-import NavBarContainer from './navbar/navbar_container';
-import Modal from './modal';
-import CampsiteIndexContainer from './campsite_index/campsite_index_container';
-import CampsiteShowContainer from './campsite_show/campsite_show_container';
-import Footer from './footer/footer';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Link, Route } from 'react-router-dom';
+
+import CampsiteIndex from './campsite_index/campsite_index';
 import CampsiteMap from './campsite_show/campsite_map';
-import UserProfileContainer from './user_profile/user_profile_container';
-import SearchBarContainer from './search/search_bar_container';
+import CampsiteShowContainer from './campsite_show/campsite_show_container';
 import DiscoverContainer from './discover/discover_container';
+import Footer from './footer/footer';
+import LoginFormContainer from './auth/login_form_container';
+import Modal from './modal';
+import NavBarContainer from './navbar/navbar_container';
+import React from 'react';
+import SearchBarContainer from './search/search_bar_container';
+import UserProfileContainer from './user_profile/user_profile_container';
 
 const App = (props) => {
   return(
@@ -21,7 +22,7 @@ const App = (props) => {
       {/* <Route path="/login" component={LoginFormContainer} /> */}
       {/* <Route path="/discover" component={CampsiteIndexContainer} /> */}
       <Route exact path="/" component={SearchBarContainer} />
-      <Route exact path="/" component={CampsiteIndexContainer} />
+      <Route exact path="/" component={CampsiteIndex} />
       <Route exact path="/campsites/:campsiteId" component={CampsiteShowContainer} />
       <ProtectedRoute path="/users/:userId" component={UserProfileContainer} />
       <Route path="/discover/" component={DiscoverContainer} />
--- components/campsite_index/campsite_index.jsx
+++ components/campsite_index/campsite_index.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
 import CampsiteIndexItem from './campsite_index_item';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-class CampsiteIndex extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+/**
+ * The `CampsiteIndex` component renders a list of campsite items based on the campsites stored in the
+ * Redux state.
+ * @returns The CampsiteIndex component is returning a list of CampsiteIndexItem components, each
+ * representing a campsite from the campsites state in the Redux store. 
+ * 
+ */
+const CampsiteIndex = () => {
+  const campsites = useSelector(state => state.entities.campsites);
 
-  componentDidMount() {
-    this.props.fetchCampsites();
-  }
+  const campsiteList = Object.values(campsites).map(campsite => {
+    return <CampsiteIndexItem key={campsite.id} campsite={campsite} />
+  });
 
-  render() {
-    const campsiteList = this.props.campsites.map(campsite => {
-      return <CampsiteIndexItem key={campsite.id} campsite={campsite} />
-    });
-
-    return (
-      <div className="campsite-list">
-        {campsiteList}
-      </div>
-    )
-  }
+  return (
+    <div className="campsite-list">
+      {campsiteList}
+    </div>
+  )
 }
 
 export default CampsiteIndex;
