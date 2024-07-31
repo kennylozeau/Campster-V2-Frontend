bedd77ff3d79729bbea034952f3de1ff1dd53069
Josiah Leon <sjosiahleon@gmail.com>
2024-07-31 11:55:51 -0700

    converted discover index to hook

--- components/discover/discover_index.jsx
+++ components/discover/discover_index.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import CampsiteIndexItem from '../campsite_index/campsite_index_item';
 
-class DiscoverIndex extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
+/**
+ * The DiscoverIndex component scrolls to the top of the page on load and renders a
+ * list of campsites based on the provided data.
+ * 
+ */
+const DiscoverIndex = ({campsites}) => {
+  
+  // when the componet is loaded, scroll to the top of the page
+  useEffect(() => {
+    // temp warning to ensure the hook is check when data is ready
+    console.warn('NO-DATA CHECK!\nDiscoverIndex was converted to a hook without data to ensure it was working!')
     window.scrollTo(0, 0);
-  }
+  }, []);
 
-  render() {
-    const campsiteList = this.props.campsites.map(campsite => {
-      return <CampsiteIndexItem key={campsite.id} campsite={campsite} />
-    });
+  // create our array of items for rendering
+  const campsiteList = campsites.map(campsite => {
+    return <CampsiteIndexItem key={campsite.id} campsite={campsite} />
+  });
 
-    return (
-      <div className="discover-list">
-        {campsiteList}
-      </div>
-    )
-  }
-}
+  
+  return (
+    <div className="discover-list">
+      {campsiteList}
+    </div>
+  )
+  
+};
 
 export default DiscoverIndex;
